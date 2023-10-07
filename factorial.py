8f9e7cf840c102ea9b6ffd4fb9410194be817e3b
Noel J Abraham <60876099+noeljabraham@users.noreply.github.com>
2023-10-07 20:07:28 +0530

    generate rec factorial values

---
+++ factorial.py
@@ -0,0 +1,23 @@
+# Recursive function to calculate factorial
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+# Main function
+def main():
+    try:
+        # Get input from the user
+        num = int(input("Enter a non-negative integer: "))
+
+        if num < 0:
+            print("Please enter a non-negative integer.")
+        else:
+            result = factorial(num)
+            print(f"The factorial of {num} is {result}")
+    except ValueError:
+        print("Invalid input. Please enter a non-negative integer.")
+
+if __name__ == "__main__":
+    main()
